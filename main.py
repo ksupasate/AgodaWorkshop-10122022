2f733b9e63b9add23aad5e45fe498c29746a38a8
Niwatchai Wangtrakuldee <niwatchai.wang@mail.kmutt.ac.th>
2022-12-10 16:19:36 +0700

    almost finish

--- main.py
+++ main.py
@@ -4,40 +4,78 @@ import pandas as pd
 conn = sqlite3.connect('test_database') 
 c = conn.cursor()
 
+# Create User Table
 c.execute('''
-          CREATE TABLE IF NOT EXISTS products
-          ([product_id] INTEGER PRIMARY KEY, [product_name] TEXT)
+          CREATE TABLE IF NOT EXISTS User
+          ([UserEmail] TEXT PRIMARY KEY, [UserPassword] TEXT , [UserFirstname] TEXT , [UserLastname] TEXT , [UserTel] TEXT)
           ''')
-          
+# Create Property Type Table
 c.execute('''
-          INSERT OR IGNORE INTO products (product_id, product_name)
+          CREATE TABLE IF NOT EXISTS PropertyType
+          ([PropertyTypeId] INTEGER PRIMARY KEY, [PropertyTypeDetail] TEXT)
+          ''')
+# Create Property Size Tanle
+c.execute('''
+          CREATE TABLE IF NOT EXISTS PropertySize
+          ([PropertySizeId] INTEGER PRIMARY KEY, [PropertySizeDetail] TEXT)
+          ''')
+# Create Property
+c.execute('''
+          CREATE TABLE IF NOT EXISTS Property
+          ([PropertyId] INTEGER PRIMARY KEY, [PropertyName] TEXT, [PropertyTypeId] INT , [PropertySizeId] INT)
+          ''')
+
+#Get User Data
+UserEmail =input('What is your UserEmail: ')
+UserPassword = input('What is your UserPassword: ')
+UserFirstname = input('What is your UserFirstname: ')
+UserLastname = input('What is your UserLastname: ')
+UserTel = input('What is your UserTel: ')
+
+#Insert Data User
+c.execute('''
+          INSERT OR IGNORE INTO User (UserEmail, UserPassword, UserFirstname, UserLastname, UserTel)
 
                 VALUES
-                (1,'Computer'),
-                (2,'Printer'),
-                (3,'Tablet'),
-                (4,'Desk'),
-                (5,'Chair')
-          ''')
+                (
+                ?,?,?,?,?
+                )
+          ''',
+                (
+                UserEmail,
+                UserPassword,
+                UserFirstname,
+                UserLastname,
+                UserTel
+                ))
 
-c.execute('''
-          INSERT OR IGNORE INTO prices (product_id, price)
+if(UserEmail != None):
+    PropertyId =input('What is your PropertyId: ')
+    PropertyName = input('What is your PropertyName: ')
+    PropertyTypeId = input('What is your PropertyTypeId: ')
+    PropertySizeId = input('What is your PropertySizeId: ')
+
+    c.execute('''
+          INSERT OR IGNORE INTO Property (PropertyId, PropertyName, PropertyTypeId, PropertySizeId)
 
                 VALUES
-                (1,800),
-                (2,200),
-                (3,300),
-                (4,450),
-                (5,150)
-          ''')
+                (
+                ?,?,?,?
+                )
+          ''',
+                (
+                PropertyId,
+                PropertyName,
+                PropertyTypeId,
+                PropertySizeId
+                ))
+
 
 c.execute('''
           SELECT
-          a.product_name,
-          b.price
-          FROM products a
-          LEFT JOIN prices b ON a.product_id = b.product_id
+          *
+          FROM User
           ''')
-df = pd.DataFrame(c.fetchall(), columns=['product_name','price'])
+df = pd.DataFrame(c.fetchall(), columns=['UserEmail','UserPassword','UserFirstname','UserLastname','UserTel'])
 print (df)
 conn.commit()
